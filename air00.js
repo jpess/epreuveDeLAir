4770c9f145c25dc492a11a810f866b8353ea25be
jpess <julien.pession@gmail.com>
2022-03-25 20:12:39 +0800

    first commit

---
+++ air00.js
@@ -0,0 +1,37 @@
+/**
+ * Split
+ */
+
+/*
+Créez un programme qui découpe une chaîne de caractères en tableau (séparateurs : espaces, tabulations, retours à la ligne).
+
+Votre programme devra utiliser une fonction prototypée comme ceci :
+ma_fonction(string_à_couper, string_séparateur) { // syntaxe selon votre langage
+	# votre algorithme
+	return (tableau)
+}
+
+Exemples d’utilisation :
+$> python exo.py “Bonjour les gars”
+Bonjour
+les
+gars
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.
+ */
+
+/******************
+ *   FUNCTIONS
+ ******************/
+
+/******************
+ * ERROR HANDLING
+ ******************/
+
+/******************
+ *      MAIN
+ ******************/
+
+/******************
+ *    DISPLAY
+ ******************/
