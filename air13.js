5d55e4893450e207c7fc6901a9496733853abd12
jpess <julien.pession@gmail.com>
2022-04-11 08:44:40 +0800

    complete

--- air13.js
+++ air13.js
@@ -29,19 +29,38 @@ Bonus : trouvez le moyen d’utiliser du vert et du rouge pour rendre réussites
 function metaCheck(){
     let result = "";
     let successCount = 0;
+    let scriptTotal = 0;
     //list all exercices in array like ['air00.js', 'air01.js', etc.]
     //for each exercice, test if file exists
-    exercices.forEach(filepath => {
-        if(fs.existsSync(filepath)){
-            result += filepath + ": success \n";
-            successCount++;
+    for (let i = 0; i < mapExercicesArguments.length; i++) {
+        const fileData = mapExercicesArguments[i];
+        if (fs.existsSync(fileData[0])){
+            for (let j = 0; j < fileData[1].length; j++) {
+                let scriptResult = runScript(fileData[0], fileData[1][j]);
+                if (scriptResult.slice(0, -1).localeCompare('error') == 0) {
+                    result += fileData[0].slice(0, -3) + " (" + (j + 1) + "/" + fileData[1].length + ") : " + "\033[31m failure \033[0m \n";
+                } else {
+                    result += fileData[0].slice(0, -3) + " (" + (j + 1) + "/" + fileData[1].length + ") : " + "\033[32m success \033[0m \n";
+                    successCount++;
+                }
+                scriptTotal++;
+            }
         } else {
-            result += filepath + ": failure \n";
+            process.exit(1);
         }
-    });
+    }
 
     //Add the Total Success data
-    result += "Total success: (" + successCount + "/" + exercices.length + ")";
+    result += "Total success: (" + successCount + "/" + scriptTotal + ")";
+    return result;
+}
+
+function runScript(filename, arguments){
+    const execSync = require('child_process').execSync;
+    const cmdLine = "node " + filename + " " + arguments.join(" ");
+    //console.log(cmdLine + "\n");
+    const result = execSync(cmdLine, { encoding: 'utf-8' });
+    //console.log(result);
     return result;
 }
 
@@ -69,10 +88,30 @@ function checkArgumentForError(args) {
  *    PARSING
  ******************/
 let args = process.argv.slice(2);  
+
 let checkResult = checkArgumentForError(args);
+
 let result = "error";
-const exercices = ['air00.js', 'air01.js', 'air02.js', 'air03.js', 'air04.js', 'air05.js', 'air06.js', 'air07.js', 'air08.js', 'air09.js', 'air10.js', 'air11.js', 'air12.js'];
+
 const fs = require("fs");
+
+const exercices = ['air00.js', 'air01.js', 'air02.js', 'air03.js', 'air04.js', 'air05.js', 'air06.js', 'air07.js', 'air08.js', 'air09.js', 'air10.js', 'air11.js', 'air12.js'];
+
+const mapExercicesArguments = [
+    ['air00.js', [['\"Bonjour les gars\"'], ['\"Comment ça va?\"'], ['\"Super test ! La folie !\"']]],
+    ['air01.js', [['\"Crevette magique dans la mer des étoiles\"', '\"la\"'], ['\"Du beurre dans les épinards\"','\"dans\"']]],
+    ['air02.js', [['Je', 'teste', 'des', 'trucs', ' ']]],
+    ['air03.js', [[1, 2, 3, 4, 5, 4, 3, 2, 1],["monsieur", "bonjour", "monsieur"]]], 
+    ['air04.js', [['\"Hello milady,   bien ou quoi ??\"']]],   
+    ['air05.js', [[1, 2, 3, 4, 5, "+2"],[10, 11, 12, 20, "-5"]]],
+    ['air06.js', [["Michel", "Albert", "Therese", "Benoit", "t"], ["Michel", "Albert", "Therese", "Benoit", "a"]]],
+    ['air07.js', [[1, 2, 4, 2],[10, 20, 30, 40, 50, 60, 70, 90, 33]]],
+    ['air08.js', [[10, 20, 30, "fusion", 15, 25, 35]]],
+    ['air09.js', [["Michel", "Albert", "Therese", "Benoit"]]],
+    ['air10.js', [["fichierTest.txt"]]],
+    ['air11.js', [[0,5], ['^',8]]],
+    ['air12.js', [[6, 5, 4, 3, 2, 1]]]];
+
 
 /******************
  *      MAIN
